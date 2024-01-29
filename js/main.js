159bc8d191a2774147366e4fe9d595669ab13be9
Mariia2809 <popovamariia2809@gmail.com>
2024-01-29 14:25:21 +0700

    Практическая работа 4- добавили объект с данными массива sizes

--- js/main.js
+++ js/main.js
@@ -7,9 +7,18 @@ let app = new Vue({
         altText: "A pair of socks",
         link:"https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
-        inventory: 100,
-        OnSale:"On Sale"
-
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
 
     }
 })
