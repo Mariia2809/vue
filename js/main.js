f391bf9a28162812f280b1b43a8ac0fa61fd1f28
Mariia2809 <popovamariia2809@gmail.com>
2024-01-30 14:39:59 +0700

    Практическая работа 7- добавление объекта с данными

--- js/main.js
+++ js/main.js
@@ -4,38 +4,53 @@ let app = new Vue({
         brand: 'Vue Mastery',
         product: "Socks",
         description: "A pair of warm, fuzzy socks.",
-        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         link:"https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        selectedVariant: 0,
+        onSale: true,
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
+
         cart: 0,
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
+        updateProduct(index) {
+            this.selectedVariant = index;
+            }
     },
     computed: {
         title() {
             return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale() {
+            if (this.onSale === true) {
+                return this.brand + ' ' + this.product + ' on sale ';
+            }
         }
     }
+
 
 
 })
